1f8ebbbd3586cb46ad482d615d625d3121829723
Kpotter9 <kjpu80129@gmail.com>
2025-04-04 20:21:49 -0400

    Backend

---
+++ masonMap/src/test/java/com/starter/masonMap/MasonMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starter.masonMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasonMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
