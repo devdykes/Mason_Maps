269cbc2a6e8247650c6a0fff79ddae23c3efa558
Kpotter9 <kjpu80129@gmail.com>
2025-04-04 22:51:31 -0400

    a

--- mason-maps/backend/masonMap/src/main/java/com/starter/masonMap/UserController.java
+++ mason-maps/backend/masonMap/src/main/java/com/starter/masonMap/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,11 +27,11 @@ public class UserController {
     public ResponseEntity<User> registerUser(@RequestBody Map<String,String> user){
         return new ResponseEntity<User>(userService.createUser(user.get("user"),user.get("password")),HttpStatus.CREATED);
 }
-@PostMapping("/login")
-public ResponseEntity<User> loginUser(@RequestBody Map<String,String> user){
-    
+    @PostMapping("/login")
+    public ResponseEntity<User> loginUser(@RequestBody Map<String,String> user){
+        
 
-    return new ResponseEntity<User>(userService.checkUser(user.get("user"),user.get("password")),HttpStatus.OK);
-}
+        return new ResponseEntity<User>(userService.checkUser(user.get("user"),user.get("password")),HttpStatus.OK);
+    }
 
-}
+    }
